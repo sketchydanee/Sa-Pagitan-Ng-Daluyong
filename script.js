a6b83d9e8d29cc6a7ce416ae221d61a9b7f7c290
sketchydane <danezekielhablo@gmail.com>
2026-05-20 04:12:03 +0800

    Finalized parallax and scrolling

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-let currentY = window.scrollY;
-let targetY = window.scrollY;
+const layers = document.querySelectorAll(".layer");
+
+const factors = [0.04, 0.08, 0.14, 0.12, 0.08, 0.18]; //sky,water,fish,surface,sharkboy,glow
+
+let currentY = 0;
+let targetY = 0;
 
 function animPageScroll() {
-  target = window.scrollY;
+  targetY = window.scrollY;
 
-  currentY += (targetY - currentY) * 0.0001;
+  currentY += (targetY - currentY) * 0.08;
 
-  window.scrollTo(0, currentY);
+  layers.forEach((layer, i) => {
+    const factor = factors[i] || 0;
 
-  requestAnimationFrame(animate);
+    layer.style.transform =
+      `translateY(${-targetY * factor}px)`;
+  });
+
+  requestAnimationFrame(animPageScroll);
 }
 
 animPageScroll();
